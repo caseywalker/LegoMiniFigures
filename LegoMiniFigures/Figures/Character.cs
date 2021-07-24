3be491c8e61051a7d3362a140a1663d4aea27912
Caseywalker <casey.walker118@gmail.com>
2021-07-24 12:27:44 -0500

    Add project files.

---
+++ LegoMiniFigures/Figures/Character.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoMiniFigures.Figures
+{
+  class Character
+  {
+    public string Name { get; set; }
+    public string Hat { get; set; }
+    public string Head { get; set; }
+    public string Torso { get; set; }
+    public string Legs { get; set; }
+    public string Accessories { get; set; }
+    public Character Opponent { get; set; }
+
+    private int _strength;
+    private int _speed;
+    private int _health;
+
+    public Character(string name, string hat, string head, string torso, string legs, string accessories)
+    {
+      Name = name;
+      Hat = hat;
+      Head = head;
+      Torso = torso;
+      Legs = legs;
+      Accessories = accessories;
+
+      _strength = 10 + Torso.Length;
+      _speed = 5 + Legs.Length;
+      _health = 100 + Head.Length;
+    }
+
+    public void Stats()
+    {
+      Console.WriteLine($"Character {Name} has a strength of {_strength}, speed rating of {_speed}, and {_health} health.");
+    }
+
+    public void Loadout()
+    {
+      Console.WriteLine($"{Name} is wearing a {Hat} hat, has a {Head} head, {Torso} for a torso, {Legs} for legs, and {Accessories} accessories.");
+    }
+
+    public void Jump()
+    {
+      Random rnd = new Random();
+      var jumpHeight = rnd.Next(1, _speed);
+      Console.WriteLine($"{Name} jumped {jumpHeight} feet.");
+    }
+
+    public void DoubleJump()
+    {
+      Random rnd = new Random();
+      var doubleJumpHeight = rnd.Next(1, _speed) * 2;
+      Console.WriteLine($"{Name} double jumped {doubleJumpHeight} feet.");
+    }
+
+    public void Attack()
+    {
+      Random rnd = new Random();
+      var attackResult = rnd.Next(1, _strength);
+      Console.Write($"{Name} attacks {Opponent.Name} for {attackResult} damage.\n");
+    }
+    public void TakeDamage()
+    {
+      throw new NotImplementedException();
+    }
+    public void Look()
+    {
+      throw new NotImplementedException();
+    }
+    public void Move()
+    {
+      throw new NotImplementedException();
+    }
+
+  }
+}
---
+++ LegoMiniFigures/Program.cs
@@ -0,0 +1,34 @@
+﻿using LegoMiniFigures.Figures;
+using System;
+
+namespace LegoMiniFigures
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Lego Minifigures");
+
+      var hulk = new Character("Hulk", "Spartan Helmet", "Green Face", "Gladiator Armor", "Ripped Pants", "War-Axe");
+      hulk.Stats();
+      hulk.Loadout();
+      hulk.Jump();
+      hulk.DoubleJump();
+
+
+      var thor = new Character("Thor", "Asgard Helm", "Eye Patch", "Leather Vest", "Red Kilt", "Mjolnir");
+      thor.Stats();
+      thor.Loadout();
+      thor.Jump();
+      thor.DoubleJump();
+
+      hulk.Opponent = thor;
+      thor.Opponent = hulk;
+
+      hulk.Attack();
+      thor.Attack();
+
+
+    }
+  }
+}
